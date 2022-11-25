a0fa99f5d7bd8219e828b38b86857e8ea9402257
swapshin <swapshin@SWAPSHIN-7420.in.oracle.com>
2022-11-25 14:12:18 +0530

    initial commit

--- MyProject/src/main/java/com/ofss/MainTest.java
+++ MyProject/src/main/java/com/ofss/MainTest.java
@@ -3,7 +3,7 @@ package com.ofss;
 public class MainTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stu
 
 		HelloWorld ob = new HelloWorld();
 		System.out.println(ob.Sayhello());
